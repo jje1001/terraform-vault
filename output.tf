8f2ad766015005112a161137f4e85764e082b53c
jje1001 <jje1001@insideinfo.co.kr>
2024-11-19 13:58:29 +0900

    test

--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
-output "kms_key_id" {
-  value = aws_kms_key.terraform_kms_key.key_id
-}
-
-output "kms_key_arn" {
-  value = aws_kms_key.terraform_kms_key.arn
-}
+# output "kms_key_id" {
+#   value = aws_kms_key.terraform_kms_key.key_id
+# }
